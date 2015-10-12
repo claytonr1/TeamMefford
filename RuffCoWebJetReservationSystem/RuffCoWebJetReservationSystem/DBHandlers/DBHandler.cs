d8a4b5fe6eab808045cf1d5c1a5ef7bdec082002
Timothy Glore <timoglor@gmail.com>
2015-10-12 01:25:12 -0500

    Changed DBHandler and updated the Typed DataSet
    Added more ease of use from the DBHandler should we need to change
    anything down the road. Also update the typed dataset to use Ruff's sql
    database. Still figuring typed datasets out though.

--- RuffCoWebJetReservationSystem/RuffCoWebJetReservationSystem/DBHandlers/DBHandler.cs
+++ RuffCoWebJetReservationSystem/RuffCoWebJetReservationSystem/DBHandlers/DBHandler.cs
@@ -10,9 +10,17 @@ namespace RuffCoJetReservation.DBHandlers
 {
     static class DBHandler
     {
+        public const string EMPLOYEES_TABLE = "RuffCoEmployees";
+        public const string PLANES_TABLE = "RuffCoPlanes";
+        public const string RESERVATIONS_TABLE = "RuffCoReservations";
+        public const string DESTINATIONS_TABLE = "RuffCoDestinations";
+        public const string GUESTS_TABLE = "RuffCoGuests";
+        public const string GUESTS_X_DEST_TABLE = "RuffCoReservationGuestXRef";
+
         private static SqlConnection sql;
-        private static SqlDataAdapter adapter;
+        private static SqlDataAdapter adapter = new SqlDataAdapter();
         private static SqlDataReader reader;
+        private static SqlCommand cmd;
 
         public static DataSet ruffCoDB = new DataSet();
 
@@ -50,11 +58,13 @@ namespace RuffCoJetReservation.DBHandlers
             //deletes current data if any to repopulate
             ruffCoDB.Clear();
 
-            loadDestinations();
-            loadEmployees();
-            loadPlanes();
-            loadGuests();
-            loadReservations();
+            //load the data in appropriate orders
+            loadPlanes(0);
+            loadEmployees(1);
+            loadDestinations(2);
+            loadGuests(3);
+            loadReservations(4);
+            loadReservationsGuestsXRef(5);
 
             //relations
             ruffCoDB.Relations.Add(ruffCoDB.Tables["employees"].Columns["employee_id"], ruffCoDB.Tables["guests"].Columns["empoyee_id"]);
@@ -62,10 +72,11 @@ namespace RuffCoJetReservation.DBHandlers
             ruffCoDB.Relations.Add(ruffCoDB.Tables["reservations"].Columns["reservations_id"], ruffCoDB.Tables["ReservationsGuestsXRef"].Columns["reservations_id"]);
             ruffCoDB.Relations.Add(ruffCoDB.Tables["guests"].Columns["guest_id"], ruffCoDB.Tables["ReservationsGuestsXRef"].Columns["guest_id"]);
             ruffCoDB.Relations.Add(ruffCoDB.Tables["planes"].Columns["plane_id"], ruffCoDB.Tables["reservations"].Columns["plane_id"]);
+            ruffCoDB.Relations.Add(ruffCoDB.Tables["reservations"].Columns["dest_id"], ruffCoDB.Tables["destinations"].Columns["dest_id"]);
 
         }
 
-        public static void loadPlanes()
+        public static void loadPlanes(int index)
         {
             try
             {
@@ -73,19 +84,21 @@ namespace RuffCoJetReservation.DBHandlers
                 {
                     ruffCoDB.Tables.Remove("planes");
                 }
-                string queryString = "SELECT * FROM planes";
-                adapter = new SqlDataAdapter(queryString, sql);
+                cmd = new SqlCommand("SELECT * FROM " + PLANES_TABLE, sql);
+                adapter.SelectCommand = cmd;
                 DataTable planes = new DataTable();
 
+                adapter.Fill(planes);
                 ruffCoDB.Tables.Add(planes);
+                ruffCoDB.Tables[index].TableName = "planes";
             }
-            catch (Exception e)
+            catch (SqlException e)
             {
                 throw e;
             }
         }
 
-        public static void loadEmployees()
+        public static void loadEmployees(int index)
         {
             try
             {
@@ -93,41 +106,21 @@ namespace RuffCoJetReservation.DBHandlers
                 {
                     ruffCoDB.Tables.Remove("employees");
                 }
-                string queryString = "SELECT * FROM employees";
-                adapter = new SqlDataAdapter(queryString, sql);
+                cmd = new SqlCommand("SELECT * FROM " + EMPLOYEES_TABLE, sql);
+                adapter.SelectCommand = cmd;
                 DataTable employees = new DataTable();
 
                 adapter.Fill(employees);
                 ruffCoDB.Tables.Add(employees);
+                ruffCoDB.Tables[index].TableName = "employees";
             }
-            catch (Exception e)
+            catch (SqlException e)
             {
                 throw e;
             }
         }
 
-        public static void loadReservations()
-        {
-            try
-            {
-                if (ruffCoDB.Tables.Contains("reservations"))
-                {
-                    ruffCoDB.Tables.Remove("reservations");
-                }
-                string queryString = "SELECT * FROM reservations";
-                adapter = new SqlDataAdapter(queryString, sql);
-                DataTable reservations = new DataTable();
-
-                adapter.Fill(reservations);
-                ruffCoDB.Tables.Add(reservations);
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-        }
-
-        public static void loadDestinations()
+        public static void loadDestinations(int index)
         {
             try
             {
@@ -135,20 +128,21 @@ namespace RuffCoJetReservation.DBHandlers
                 {
                     ruffCoDB.Tables.Remove("destinations");
                 }
-                string queryString = "SELECT * FROM destinations";
-                adapter = new SqlDataAdapter(queryString, sql);
+                cmd = new SqlCommand("SELECT * FROM " + DESTINATIONS_TABLE, sql);
+                adapter.SelectCommand = cmd;
                 DataTable destinations = new DataTable();
 
                 adapter.Fill(destinations);
                 ruffCoDB.Tables.Add(destinations);
+                ruffCoDB.Tables[index].TableName = "destinations";
             }
-            catch (Exception e)
+            catch (SqlException e)
             {
                 throw e;
             }
         }
 
-        public static void loadGuests()
+        public static void loadGuests(int index)
         {
             try
             {
@@ -156,20 +150,43 @@ namespace RuffCoJetReservation.DBHandlers
                 {
                     ruffCoDB.Tables.Remove("guests");
                 }
-                string queryString = "SELECT * FROM guests";
-                adapter = new SqlDataAdapter(queryString, sql);
+                cmd = new SqlCommand("SELECT * FROM " + GUESTS_TABLE, sql);
+                adapter.SelectCommand = cmd;
                 DataTable guests = new DataTable();
 
                 adapter.Fill(guests);
                 ruffCoDB.Tables.Add(guests);
+                ruffCoDB.Tables[index].TableName = "guests";
             }
-            catch (Exception e)
+            catch (SqlException e)
             {
                 throw e;
             }
         }
 
-        public static void loadReservationsGuestsXRef()
+        public static void loadReservations(int index)
+        {
+            try
+            {
+                if (ruffCoDB.Tables.Contains("reservations"))
+                {
+                    ruffCoDB.Tables.Remove("reservations");
+                }
+                cmd = new SqlCommand("SELECT * FROM " + RESERVATIONS_TABLE, sql);
+                adapter.SelectCommand = cmd;
+                DataTable reservations = new DataTable();
+
+                adapter.Fill(reservations);
+                ruffCoDB.Tables.Add(reservations);
+                ruffCoDB.Tables[index].TableName = "reservations";
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+        }
+
+        public static void loadReservationsGuestsXRef(int index)
         {
             try
             {
@@ -177,14 +194,15 @@ namespace RuffCoJetReservation.DBHandlers
                 {
                     ruffCoDB.Tables.Remove("ReservationsGuestsXRef");
                 }
-                string queryString = "SELECT * FROM ReservationsGuestsXRef";
-                adapter = new SqlDataAdapter(queryString, sql);
+                cmd = new SqlCommand("SELECT * FROM " + GUESTS_X_DEST_TABLE, sql);
+                adapter.SelectCommand = cmd;
                 DataTable ReservationsGuestsXRef = new DataTable();
 
                 adapter.Fill(ReservationsGuestsXRef);
                 ruffCoDB.Tables.Add(ReservationsGuestsXRef);
+                ruffCoDB.Tables[index].TableName = "ReservationsGuestsXRef";
             }
-            catch (Exception e)
+            catch (SqlException e)
             {
                 throw e;
             }
@@ -194,7 +212,7 @@ namespace RuffCoJetReservation.DBHandlers
         {
             try
             {
-                
+
             }
             catch (Exception e)
             {
@@ -206,7 +224,7 @@ namespace RuffCoJetReservation.DBHandlers
         {
             try
             {
-                
+
             }
             catch (Exception e)
             {
@@ -218,7 +236,7 @@ namespace RuffCoJetReservation.DBHandlers
         {
             try
             {
-                
+
             }
             catch (Exception e)
             {
@@ -230,7 +248,7 @@ namespace RuffCoJetReservation.DBHandlers
         {
             try
             {
-                
+
             }
             catch (Exception e)
             {
@@ -242,7 +260,7 @@ namespace RuffCoJetReservation.DBHandlers
         {
             try
             {
-                
+
             }
             catch (Exception e)
             {
@@ -254,7 +272,7 @@ namespace RuffCoJetReservation.DBHandlers
         {
             try
             {
-                
+
             }
             catch (Exception e)
             {
