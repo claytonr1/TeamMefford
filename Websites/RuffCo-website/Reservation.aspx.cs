fc24fcbf25d8756e12caacc450de7954cdc6c734
KevinMagers <km4695@gmail.com>
2015-12-03 14:49:03 -0600

    Changes during meeting to fix selected planes/destinations

--- Websites/RuffCo-website/Reservation.aspx.cs
+++ Websites/RuffCo-website/Reservation.aspx.cs
@@ -17,34 +17,51 @@ namespace RuffCoJetReservationSystem
     public partial class Reservation : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-                  
+        {    
                 //login check
                 CookieHandler.checkLogin();
                 CookieHandler.clearResultsCookies(); //clear cookies if second reservation or more this session
                 //this populates the name of the current user into the Label1 textbox
                 Label1.Text = CookieHandler.getUserFullName();
 
-                Dictionary<int, Plane> planes = (Dictionary<int, Plane>)Session["planes"];
+                //destinationDropDownList.Items.Clear();
+                //jetsDropDownList.Items.Clear();
 
-                destinationDropDownList.Items.Clear();
-                jetsDropDownList.Items.Clear();
+                //String[] destList = DBDestinations.getDestinationsList().ToArray();
+                //foreach (string s in destList)
+                //{
+                //    destinationDropDownList.Items.Add(s); //automatically adds database items to dropdown list -ksm
+                //}
 
-                String[] destList = DBDestinations.getDestinationsList().ToArray();
-                foreach (string s in destList)
+                //foreach (var pair in planes)
+                //{
+                //    int key = pair.Key;
+                //    Plane p = pair.Value;
+                //    if (p.isAvailable())
+                //    {
+                //        jetsDropDownList.Items.Add(p.name); //automatically adds available jets to dropdownlist -ksm 
+                //    }    
+                //}
+                if (!IsPostBack)
                 {
-                    destinationDropDownList.Items.Add(s); //automatically adds database items to dropdown list -ksm
-                }
+                    Dictionary<int, Plane> planes = (Dictionary<int, Plane>)Session["planes"];
 
-                foreach (var pair in planes)
-                {
-                    int key = pair.Key;
-                    Plane p = pair.Value;
-                    if (p.isAvailable())
+                    String[] destList;
+                    destList = DBDestinations.getDestinationsList().ToArray();
+                    foreach (string s in destList)
                     {
-                        jetsDropDownList.Items.Add(p.name); //automatically adds available jets to dropdownlist -ksm 
-                    }    
+                        destinationDropDownList.Items.Add(s); //automatically adds database items to dropdown list -ksm
+                    }
+                    String[] planeList;
+                    planeList = DBPlanes.PlanesList().ToArray();
+                    foreach (string s in planeList)
+                    {
+                        jetsDropDownList.Items.Add(s); //automatically adds database items to dropdown list -ksm
+
+                    }
                 }
+
+
           }
 
         protected void Button1_Click(object sender, EventArgs e)
@@ -69,7 +86,7 @@ namespace RuffCoJetReservationSystem
             DBReservations.RegisterReservation(DBPlanes.getID(jet), CookieHandler.getID(), DBDestinations.getID(dest), date);
 
             //sets cookies for use in results page
-            CookieHandler.setCookie("jet", jetsDropDownList.SelectedValue);
+            CookieHandler.setCookie("jet", DBPlanes.getName(DBPlanes.getID(jet)));
             CookieHandler.setCookie("dest", destinationDropDownList.SelectedValue);
             CookieHandler.setCookie("date", date.ToString());
             //change page to results
